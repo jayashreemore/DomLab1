d562e6a5a90b13695aa543963bc7bea8435c9750
jayashreemore <jayashreematre@gmail.com>
2024-03-14 16:31:36 -0400

    Part 2 is done Navbar on top

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 //DOM Lab Part 1
+// Select and cache the <main> element in a variable named mainEl.
+// Set the background color of mainEl to the value stored in the --main-bg CSS custom property.
+// Hint: Assign a string that uses the CSS var() function like this: 'var(--main-bg)'.
+// Set the content of mainEl to <h1>DOM Manipulation</h1>. There are a variety of ways to do this; pick whichever one that you think works best in this situation.
+// Add a class of flex-ctr to mainEl.
 //create main element , queryselector ,for bg color style it,create h1, bring it in center 
 // 1
 let mainEl = document.querySelector('main');
+console.log(mainEl);
 //2
 mainEl.style.backgroundColor = "var(--main-bg)";
 //3 
@@ -17,9 +23,10 @@ mainEl.classList.add("flex-ctr");
 // Set the background color of topMenuEl to the value stored in the --top-menu-bg CSS custom property.
 // Add a class of flex-around to topMenuEl.
 
-// const topMenuEl = document.getElementById('top-menu');
-// topMenuEl.style.height = '100%';
-// topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
-// topMenuEl.classList.add("flex-around");
+const topMenuEl = document.getElementById('top-menu');
+console.log(topMenuEl);
+topMenuEl.style.height = '100%';
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+topMenuEl.classList.add("flex-around");
 
 
